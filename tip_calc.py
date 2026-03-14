00a69fb5030179337ef6d581bfc937962a1ebef2
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-03-14 02:52:26 -0700

    Updated tip_calc.py

--- tip_calc.py
+++ tip_calc.py
@@ -1,8 +1,16 @@
+## Python based tip calculator:
+#input bill
 bill = float(input("enter total bill amount:"))
+
+#input tip_percent:
 tip_percent = float(input("enter tip percentage:"))
 
+#formulising tip 
 tip = bill * (tip_percent/100)
+
+##total
 total = bill + tip
 
+#final step of printing tip amount and total amount to pay:
 print("tip amount:",tip)
 print("total amount to pay:",total)
